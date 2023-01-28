3744fd9209573afa9927e9aef88485dd1bcae722
naturebug07 <97745575+naturebug07@users.noreply.github.com>
2023-01-28 19:13:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# interview
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-3wzbqh)
---
+++ index.js
@@ -0,0 +1,32 @@
+// 1st assignment
+// add all the count values in res using for loop and console the sum as output.
+const res = [
+  {
+    count: 1,
+  },
+  {
+    count: 2,
+  },
+  {
+    count: 3,
+  },
+];
+
+// 2nd assignment
+// add all the count values in response using for loop and console the sum as output.
+
+const response = {
+  maran: {
+    count: 1,
+  },
+  gas: {
+    count: 2,
+  },
+  prelude: {
+    count: 5,
+  },
+  keys: ['maran', 'gas', 'prelude'],
+};
+
+// 3rd assignment
+// change the 'HI' Text color on clicking of button.
