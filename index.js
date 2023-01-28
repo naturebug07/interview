d015b8f854395b19e2e19eba7943105a0eb8a55c
naturebug07 <97745575+naturebug07@users.noreply.github.com>
2023-01-29 00:29:10 +0530

    2 files modified

--- index.js
+++ index.js
@@ -11,10 +11,14 @@ const res = [
     count: 3,
   },
 ];
+var total = 0;
+for (let index = 0; index < res.length; index++) {
+  total = total + res[index].count;
+}
+console.log('the sum of count is :' + ' ' + total);
 
 // 2nd assignment
 // add all the count values in response using for loop and console the sum as output.
-
 const response = {
   maran: {
     count: 1,
@@ -28,5 +32,18 @@ const response = {
   keys: ['maran', 'gas', 'prelude'],
 };
 
+let sum=0;
+
+for (let i in objArray){
+  console.log(objArray[i])
+  if (objArray[i]["count"]==parseInt(objArray[i]["count"])){
+    sum =sum+objArray[i]["count"]
+  }
+}
+console.log('the sum of count is :' + ' ' +sum);
 // 3rd assignment
 // change the 'HI' Text color on clicking of button.
+document.getElementById("changeColor").onclick = function()
+{
+  document.getElementById("changeColor").style.color = 'green';
+}
